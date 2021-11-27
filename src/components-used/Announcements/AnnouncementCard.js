d14b8edfaa7de540674d926b50da50f80cb737e8
Meghana Varanasi <meghana.cosmos@gmail.com>
2021-11-27 19:54:30 +0530

    reply try

--- src/components-used/Announcements/AnnouncementCard.js
+++ src/components-used/Announcements/AnnouncementCard.js
@@ -15,12 +15,14 @@ import {
   Typography,
   CardHeader,
   CardContent,
-  Avatar
+  Avatar,
+  Modal
 } from '@mui/material';
 // utils
 import React from 'react';
 import { setDoc, doc, getDoc, getDocs, collection, query } from 'firebase/firestore';
 import moment from 'moment';
+import ReplyModal from './ReplyModal';
 // ----------------------------------------------------------------------
 
 AnnouncementCard.propTypes = {
@@ -29,27 +31,56 @@ AnnouncementCard.propTypes = {
 
 export default function AnnouncementCard({ data }) {
   const deadlineDate = data.deadline?.toDate();
+  const [open, setOpen] = React.useState(false);
   const publishedAtDate = data.publishedDate?.toDate();
+  const [title, setTitle] = React.useState(data?.title);
+  const handleClose = () => setOpen(false);
   return (
-    <Card sx={{ p: 1, mb: 2, mt: 2, minWidth: '100%' }}>
-      <CardContent>
-        <Box sx={{ minWidth: 240 }}>
-          <Typography variant="h4">
-            {data.type !== 'assignment' && <Avatar src={data.photoURL} />}
-            {data.type === 'assignment' ? `${data.name} ` : data.title}
+    <>
+      <Card sx={{ p: 1, mb: 2, mt: 2, minWidth: '100%' }}>
+        <CardContent>
+          <Box sx={{ minWidth: 240 }}>
+            <Typography variant="h4">
+              {data.type !== 'assignment' && <Avatar src={data.photoURL} />}
+              {data.type === 'assignment' ? `${data.name} ` : data.title}
+            </Typography>
+          </Box>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+            {data.type === 'assignment'
+              ? `Score:${data.totalScore} | Deadline: ${deadlineDate}`
+              : data.message}
           </Typography>
-        </Box>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-          {data.type === 'assignment'
-            ? `Score:${data.totalScore} | Deadline: ${deadlineDate}`
-            : data.message}
-        </Typography>
-        <Box sx={{ flexGrow: 1 }} />
-
-        <Typography variant="caption" sx={{ flexShrink: 0, color: 'text.secondary' }}>
-          {`Author : ${data.author} | Published at : ${publishedAtDate}`}
-        </Typography>
-      </CardContent>
-    </Card>
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="caption" sx={{ flexShrink: 0, color: 'text.secondary' }}>
+            {`Author : ${data.author} | Published at : ${publishedAtDate}`}
+          </Typography>
+          {/* {data.type !== 'assignment' && (
+            <>
+              {' '}
+              <Button
+                onClick={() => {
+                  setTitle(data.title);
+                  setOpen(true);
+                }}
+              >
+                {' '}
+                Reply{' '}
+              </Button>
+              {data.replies.length > 0 && <hr />}
+            </> */}
+          {/* )} */}
+          {/* {data.type !== 'assignment' &&
+            data.replies.map((item) => <div key={JSON.stringify(item.postedAt)}>{item.reply}</div>)} */}
+        </CardContent>
+      </Card>
+      {/* <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ReplyModal title={title} setOpen={setOpen} />
+      </Modal> */}
+    </>
   );
 }
---
+++ src/components-used/Announcements/ReplyModal.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Box, Button, Card, TextField, Grid } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+// components
+import { getDoc, doc, arrayUnion } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { setDoc, Timestamp, updateDoc } from '@firebase/firestore';
+import { db, auth } from '../../firebase/initFirebase';
+import { MyContext } from '../../utils/context';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  boxShadow: 24,
+  p: 4
+};
+function AnnouncementModal({ setOpen, title }) {
+  const { classSelected } = React.useContext(MyContext);
+  const [curUser, setCurUser] = React.useState(null);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurUser(user);
+    }
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const reply = data.get('reply');
+    const docRef = doc(db, 'classes', classSelected);
+    const docData = {
+      announcements: arrayUnion(
+        ...[
+          {
+            type: 'conversation',
+            title,
+            replies: arrayUnion(
+              ...[
+                {
+                  reply,
+                  author: curUser?.email,
+                  photoURL: curUser?.photoURL,
+                  postedAt: Timestamp.fromDate(new Date())
+                }
+              ]
+            )
+          }
+        ]
+      )
+    };
+    updateDoc(docRef, docData, { merge: true });
+    setOpen(false);
+  };
+  return (
+    <>
+      <Card sx={style}>
+        <Box component="form" onSubmit={(e) => handleSubmit(e)}>
+          <Typography variant="h4" sx={{ mt: 3, textAlign: 'center' }}>
+            {' '}
+            Post a Reply to {title}
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                label="reply"
+                id="reply"
+                name="reply"
+                fullWidth
+                style={{ width: '100%' }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" fullWidth>
+                Post
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Card>
+    </>
+  );
+}
+
+export default AnnouncementModal;
